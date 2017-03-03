e08851c0d72d3840ab1a24f20c189d346953703c
xiaochun <xchun90@163.com>
2017-03-03 21:35:19 +0800

    optimize page render

--- server/src/main/resources/static/assets/js/plugin.js
+++ server/src/main/resources/static/assets/js/plugin.js
@@ -1,63 +1,70 @@
 $(function(){
-    $.get("../plugins",function(e){
-        var pluginVersion = new Vue({
-            el : '#pluginVersion',
-            data : {
-                message : e.pluginVersion
-            },
-        })
 
-        var allPlugins = new Vue({
-            el : '#allPlugins',
-            data : {
-                plugins : e.plugins,
-                usedPlugins : e.usedPlugins,
-                unusedPlugins : e.unusedPlugins,
-            },
-            methods:{
-                getStartHref:function(val){
-                    return "../"+val+"/start"
-                },
-                getViewHref:function(val){
-                    return "../"+val+"/"
-                },
-                getStopHref:function(val){
-                    return "../"+val+"/stop"
-                },
-                val(val){
-                    return val;
-                },
-                stop:function(name){
-                    $.get("../"+name+"/stop",function(e){
-                        if(e.code == 0){
-                            $.gritter.add({
-                                title: "   "+e.message,
-                                class_name: 'gritter-success' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
-                            });
-                        }else{
-                            $.gritter.add({
-                                title: e.message,
-                                class_name: 'gritter-error' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
-                            });
-                        }
-                    })
-                },
-                uninstall:function(name){
-                    $.get("../"+name+"/uninstall",function(e){
-                        if(e.code == 0){
-                            $.gritter.add({
-                                title:"   "+ e.message,
-                                class_name: 'gritter-success' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
-                            });
-                        }else{
-                            $.gritter.add({
-                                title: e.message,
-                                class_name: 'gritter-error' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
-                            });
-                        }
-                    })
+
+var allPlugins = new Vue({
+    el : '#plugin',
+    data : {
+        version : '',
+        plugins : [],
+        usedPlugins : [],
+        unusedPlugins : [],
+    },
+    mounted: function(){
+        renderData();
+    },
+    methods:{
+        getStartHref:function(val){
+            return "../"+val+"/start"
+        },
+        getViewHref:function(val){
+            return "../"+val+"/"
+        },
+        getStopHref:function(val){
+            return "../"+val+"/stop"
+        },
+        val(val){
+            return val;
+        },
+        stop:function(name){
+            $.get("../"+name+"/stop",function(e){
+                if(e.code == 0){
+                    $.gritter.add({
+                        title: "   "+e.message,
+                        class_name: 'gritter-success' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
+                    });
+                }else{
+                    $.gritter.add({
+                        title: e.message,
+                        class_name: 'gritter-error' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
+                    });
                 }
-            }
-        })
+                renderData();
+            })
+        },
+        uninstall:function(name){
+            $.get("../"+name+"/uninstall",function(e){
+                if(e.code == 0){
+                    $.gritter.add({
+                        title:"   "+ e.message,
+                        class_name: 'gritter-success' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
+                    });
+                }else{
+                    $.gritter.add({
+                        title: e.message,
+                        class_name: 'gritter-error' + (!$('#gritter-light').get(0).checked ? ' gritter-light' : ''),
+                    });
+                }
+                renderData();
+            })
+        }
+    }
+})
+function renderData(){
+    $.get("../plugins",function(e){
+        allPlugins.$set(allPlugins,'version','v'+e.pluginVersion);
+        allPlugins.$set(allPlugins,'plugins',e.plugins);
+        allPlugins.$set(allPlugins,'usedPlugins',e.usedPlugins);
+        allPlugins.$set(allPlugins,'unusedPlugins',e.unusedPlugins);
     })
-});
+};
+})
