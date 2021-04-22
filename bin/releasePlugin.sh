2303c6cb22805423f5880140e361e60fec54d756
xiaochun <xchun90@163.com>
2021-04-23 02:31:55 +0800

    Try fix

--- bin/releasePlugin.sh
+++ bin/releasePlugin.sh
@@ -1,7 +1,7 @@
 outFolder=${1}
 pluginJarName="plugin-core-${2}.jar"
-./mvnw clean install -f "plugin-common/pom.xml"
-./mvnw clean compile assembly:single -f "plugin-core/pom.xml"
+./mvnw -U clean install -f "plugin-common/pom.xml"
+./mvnw -U clean compile assembly:single -f "plugin-core/pom.xml"
 mkdir -p ${1}
 cp plugin-core/target/plugin-core.jar ${1}/$pluginJarName
 cp plugin-core/target/plugin-core.jar ${1}/plugin-core.jar
