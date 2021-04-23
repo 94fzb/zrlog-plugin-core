16e59a9d1c5f8c99709ba03876746fc86ffe9853
xiaochun <xchun90@163.com>
2021-04-23 13:55:28 +0800

    Fix package error

--- bin/releasePlugin.sh
+++ bin/releasePlugin.sh
@@ -1,7 +1,6 @@
 outFolder=${1}
 pluginJarName="plugin-core-${2}.jar"
-./mvnw -U clean install -f "plugin-common/pom.xml"
-./mvnw -U clean compile assembly:single -f "plugin-core/pom.xml"
+./mvnw -U clean package
 mkdir -p ${1}
 cp plugin-core/target/plugin-core.jar ${1}/$pluginJarName
 cp plugin-core/target/plugin-core.jar ${1}/plugin-core.jar
