75e5b733e78f0ae61c3f3d925ef557f4b9d4f794
xiaochun <xchun90@163.com>
2023-12-17 19:57:37 +0800

    fix

--- zrlog-plugin-core/src/main/java/com/zrlog/plugincore/server/config/PluginInterceptor.java
+++ zrlog-plugin-core/src/main/java/com/zrlog/plugincore/server/config/PluginInterceptor.java
@@ -7,7 +7,7 @@ import com.hibegin.http.server.api.Interceptor;
 public class PluginInterceptor implements Interceptor {
     @Override
     public boolean doInterceptor(HttpRequest httpRequest, HttpResponse httpResponse) {
-        if(httpRequest.getUri().startsWith("/static/") && httpRequest.getUri().startsWith("/admin/plugins/static/")){
+        if(httpRequest.getUri().startsWith("/static/") || httpRequest.getUri().startsWith("/admin/plugins/static/")){
             httpResponse.addHeader("Cache-Control", "max-age=31536000, immutable"); // 1 年的秒数
         }
         httpResponse.addHeader("Connection", "close");
