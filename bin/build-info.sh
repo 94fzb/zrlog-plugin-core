bcabea476c48167c7f5d61b48efcc52d2f547e8a
xiaochun <xchun90@163.com>
2024-01-05 22:56:48 +0800

    address

--- bin/build-info.sh
+++ bin/build-info.sh
@@ -5,4 +5,4 @@ length=7
 commitId=$(git log --format="%H" -n 1)
 buildNumber=$(git rev-list --all --count)
 buildId=$(expr substr ${commitId} 1 ${length})
-echo -e "server.port=9090\nversion=${clean_version}\npluginJvmArgs=-Dfile.encoding=UTF-8 -Xms4m -Xmx32m\nbuildId=${buildId}\nbuildTime=${Date}\nbuildNumber=${buildNumber}" > zrlog-plugin-core/src/main/resources/conf.properties
+echo -e "server.port=9090\nversion=${clean_version}\npluginJvmArgs=-Dfile.encoding=UTF-8 -Xms4m -Xmx32m\nbuildId=${buildId}\nbuildTime=${Date}\nbuildNumber=${buildNumber}" > src/main/resources/conf.properties
