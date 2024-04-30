e446cdbd286ac86082261e4e9965f4013758a6a9
xiaochun <xchun90@163.com>
2024-04-30 14:25:40 +0800

    Fix missing

--- src/main/frontend/src/AppBase.tsx
+++ src/main/frontend/src/AppBase.tsx
@@ -10,8 +10,10 @@ class AppBase extends React.Component {
     render() {
         return (
             <Routes>
-                <Route path='*/downloadResult' element={<DownloadResult/>}/>
-                <Route path='*/pluginStarted' element={<PluginStarted/>}/>
+                <Route path='/admin/plugins/downloadResult' element={<DownloadResult/>}/>
+                <Route path='/downloadResult' element={<DownloadResult/>}/>
+                <Route path='/admin/plugins/pluginStarted' element={<PluginStarted/>}/>
+                <Route path='/pluginStarted' element={<PluginStarted/>}/>
                 <Route path="*" element={<CoreIndex/>}/>
             </Routes>
         );
