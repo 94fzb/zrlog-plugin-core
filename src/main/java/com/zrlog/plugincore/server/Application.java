7568f43adfe715af7319b2118b4f7c9fe5aeac95
xiaochun <xchun90@163.com>
2024-05-19 20:10:36 +0800

    Try fix logging error

--- src/main/java/com/zrlog/plugincore/server/Application.java
+++ src/main/java/com/zrlog/plugincore/server/Application.java
@@ -18,16 +18,14 @@ import com.zrlog.plugincore.server.util.ListenWebServerThread;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.logging.Logger;
 
 public class Application {
 
-    private static final Logger LOGGER = LoggerUtil.getLogger(Application.class);
 
     public static void main(String[] args) throws IOException {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %5$s%6$s%n");
         if (args != null && args.length > 0) {
-            LOGGER.info("args = " + Arrays.toString(args));
+            LoggerUtil.getLogger(Application.class).info("args = " + Arrays.toString(args));
         }
         Integer serverPort = (args != null && args.length > 0) ? Integer.parseInt(args[0]) : 9089;
         int masterPort = (args != null && args.length > 1) ? Integer.valueOf(args[1]) : ConfigKit.getServerPort();
