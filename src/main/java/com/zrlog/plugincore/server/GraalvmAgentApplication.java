45851bf4cdcb17fc87174147d633046bfcb163b6
xiaochun <xchun90@163.com>
2024-05-29 00:26:09 +0800

    Try fix missing runType

--- src/main/java/com/zrlog/plugincore/server/GraalvmAgentApplication.java
+++ src/main/java/com/zrlog/plugincore/server/GraalvmAgentApplication.java
@@ -4,27 +4,21 @@ import com.google.gson.Gson;
 import com.hibegin.common.util.Pid;
 import com.hibegin.http.server.util.NativeImageUtils;
 import com.hibegin.http.server.util.PathUtil;
-import com.zrlog.plugin.data.codec.FileDesc;
-import com.zrlog.plugin.data.codec.HttpRequestInfo;
-import com.zrlog.plugin.message.Plugin;
+import com.zrlog.plugin.common.PluginNativeImageUtils;
 import com.zrlog.plugincore.server.config.PluginCore;
 import com.zrlog.plugincore.server.config.PluginCoreSetting;
 import com.zrlog.plugincore.server.config.PluginVO;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class GraalvmAgentApplication {
 
     public static void main(String[] args) throws IOException {
+        PluginNativeImageUtils.usedGsonObject();
         new Gson().toJson(new PluginVO());
-        new Gson().toJson(new FileDesc());
-        new Gson().toJson(new HashMap<>());
-        new Gson().toJson(new HttpRequestInfo());
         new Gson().toJson(new PluginCoreSetting());
         new Gson().toJson(new PluginCore());
-        new Gson().toJson(new Plugin());
         Application.init();
         Pid.get();
         String basePath = System.getProperty("user.dir").replace("/target", "");
