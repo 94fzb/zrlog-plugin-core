82c23ef4ba021eaaa9f70a5d91eccb4e8641200f
xiaochun <90xchun@gmail.com>
2025-03-23 23:00:18 +0800

    Try update file name

--- bin/package-native-github.sh
+++ bin/package-native-github.sh
@@ -8,15 +8,18 @@ sh bin/build-info.sh
 ./mvnw clean package
 ./mvnw -Pnative -Dagent exec:exec@java-agent -U
 ./mvnw -Pnative package
-binName=static-plus
+binName="plugin-core"
 if [ -f "target/${binName}.exe" ];
 then
   echo "window"
   mv "target/${binName}.exe" "${basePath}/${binName}-Windows-$(uname -m).exe"
   exit 0;
 fi
-if [ -f "target/${binName}" ];
+if [[ "$(uname -s)" == "Linux" ]];
 then
-  echo "unix"
+  echo "Linux"
+  mv target/${binName} ${basePath}/${binName}-$(uname -s)-$(dpkg --print-architecture).bin
+else
+  echo "MacOS"
   mv target/${binName} ${basePath}/${binName}-$(uname -s)-$(uname -m).bin
 fi
